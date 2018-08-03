4c1dfd7793ea91c01ae4d17c63881743972a745c
Morten Hels <mortenhels@gmail.com>
2018-08-03 09:17:44 +0200

    fix

--- foo.sh
+++ foo.sh
@@ -1,3 +1,5 @@
+DIR_HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+echo $DIR_HERE
 echo running abc
 git clone git@github.com:mchels/format_ticks.git format_ticks2
 echo running abc
